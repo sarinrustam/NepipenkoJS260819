4cdd4898e6140cdabbf85f0d95bd68023f3c133f
Rustam Sarin <lorienas@gmail.com>
2019-07-24 11:31:43 +0300

    Добавление элемента в конец

---
+++ trash.js
@@ -0,0 +1,9 @@
+let goods = ["Телевизоры", "Телефоны", "Ноутбуки", "Холодильники", "Пылесосы"];
+
+function lastPush(array) {
+    return array.push("Компьютеры");
+
+};
+
+console.log(lastPush(goods));
+console.log(goods);
